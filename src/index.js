a80b753435a9f3af4170ae0e34eebe822d3b5771
Francisco Nascimento <fmln@cin.ufpe.br>
2020-12-13 03:04:13 -0300

    criacao e estilizacao da barra de navegacao

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import SudokuSolver from './sudoku-solver/sudoku-solver'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+
+function Icon(props){
+    let icon = "icon " + props.icon;
+    if(props.icon === " bars"){
+        return <span  ><FontAwesomeIcon className={icon} onClick={() => props.onClick(props.icon)} icon={faBars} /></span>
+    }else{
+        return <span><FontAwesomeIcon className={icon} onClick={() => props.onClick(props.icon)} icon={faTimes} /></span>
+    }
+}
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            icon: " bars",
+            displayLinks: "topnav ",
+        }
+    };
+
+    handleClick(icon){
+
+        if(icon === " bars")
+            this.setState({icon: " close ", displayLinks: "topnav display-links"});
+        else    
+            this.setState({icon: " bars", displayLinks: "topnav "});       
+        console.log(this.state.displayLinks);
+    }
+    render(){
+        return (
+            <Router>
+                <div className={this.state.displayLinks}>
+                    <Link to={'/#'}> Home </Link>
+                    <Link to={'/#'} className="algo"> 8 Queens </Link>
+                    <Link to={'/sudoku'} className="algo"> Sudoku </Link>
+                    <Icon icon={this.state.icon} onClick={(icon) => this.handleClick(icon)} />
+
+                     { /*add onlick for both of tem*/}
+
+                </div>
+              <Switch>
+                  {/* <Route path='/' component={Home} /> */}
+                  <Route path='/sudoku' component={SudokuSolver} />
+              </Switch>
+          </Router>
+        )
+    };
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Home/>,
+    // <SudokuSolver/>,
+    document.getElementById('root')
+)
